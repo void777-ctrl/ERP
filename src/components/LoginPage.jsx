fc904edb28669180726415e62079c219c2d7c5a7
void-ctrl777 <mayurmahajan644@gmail.com>
2025-02-04 10:54:35 +0530

    Change in the outline and border of input fields

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -5,8 +5,8 @@ const LoginPage = () => {
     <>
         <div className='h-screen flex justify-center items-center'>
             <form action="" className='shadow-2xl px-8 py-8 rounded-2xl w-full max-w-[350px]'>
-                <img src={SumagoLogo} alt="SumagoLogo" className='mx-auto mb-4' />
-                <select name="" id="" className='block mx-auto shadow px-2 py-1 mb-4'>
+                <img src={SumagoLogo} alt="SumagoLogo" className='mx-auto mb-8 ' />
+                <select name="" id="" className='block mx-auto shadow px-4 py-2 mb-6'>
                     <option value="Select" hidden>Select Login</option>
                     <option value="Student">Student</option>
                     <option value="Admin">Admin</option>
@@ -14,14 +14,14 @@ const LoginPage = () => {
                     <option value="BDE">BDE</option>
                 </select>
                 {/* Email Input */}
-                <div className='mb-4'>
-                    <label htmlFor="email" className='font-semibold'>UserID or Email</label>
-                    <input type="email" id='email' className='border rounded w-full px-4 py-1' />
+                <div className='mb-6'>
+                    <label htmlFor="email" className='font-semibold block mb-2'>UserID or Email</label>
+                    <input type="email" id='email' className='border border-slate-400 focus:outline-blue-400 rounded w-full px-4 py-1.5' />
                 </div>
                 {/* Password Input */}
-                <div className='mb-4'>
-                    <label htmlFor="password" className='font-semibold'>Password</label>
-                    <input type="password" id='password' className='border rounded w-full px-4 py-1' />
+                <div className='mb-6'>
+                    <label htmlFor="password" className='font-semibold block mb-2'>Password</label>
+                    <input type="password" id='password' className='border border-slate-400 focus:outline-blue-400 rounded w-full px-4 py-1.5' />
                 </div>
                 {/* Login Button */}
                 <button className='block mx-auto bg-blue-600 hover:bg-blue-700 cursor-pointer text-white font-semibold rounded px-4 py-1.5'>Login</button>
